ca66cf7475d604936289f1738f8a6325687c88bd
bentoco <bentoco@outlook.com.br>
2020-05-25 17:08:25 -0300

    full project

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
@@ -26,10 +27,18 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     const {error} = loginValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-});
 
+    const userExist = await User.findOne({ email: req.body.email });
+    if (!userExist) return res.status(400).send('email or password is wrong.');
+
+    const validPass = await bcrypt.compare(req.body.password, userExist.password);
+    if(!validPass) return res.status(400).send('invalid password');
+
+    const token = jwt.sign({ _id: userExist._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+});
 
 module.exports = router;
